9e128a937a639dc053435fa01c162ce2841acab0
Mario-Devv <marioodevv@gmail.com>
2025-07-25 13:47:40 +0200

    adding relations

--- capitulo-cero/backend/Models/books.js
+++ capitulo-cero/backend/Models/books.js
@@ -3,9 +3,10 @@ import mongoose from 'mongoose'
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   auth: { type: String },
-  category: { type: String },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
   editorial: { type: String },
-  publishDate: { type: Date }
+  publishDate: { type: Date },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 })
 
 export const BookModel = mongoose.model('Book', bookSchema)
